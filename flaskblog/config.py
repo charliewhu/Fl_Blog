0df683606d19d35dee26513a02aa4066390dd245
charl <charl@DESKTOP-A4DK2O9>
2021-09-01 15:57:45 +0100

    first app commit

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,14 +2,22 @@ import os
 import dotenv
 
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 class Config:
-    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+    
     dotenv_file = os.path.join(BASE_DIR, ".env")
     if os.path.isfile(dotenv_file):
         dotenv.load_dotenv(dotenv_file)
 
     SECRET_KEY = os.environ['SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '')\
+        .replace('postgres://', 'postgresql://')\
+        or 'sqlite:///' + \
+        os.path.join(BASE_DIR, 'app.db')
+
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+
     MAIL_SERVER = os.environ['MAIL_SERVER']
     MAIL_PORT = os.environ['MAIL_PORT']
     MAIL_USE_TLS = os.environ['MAIL_USE_TLS']
